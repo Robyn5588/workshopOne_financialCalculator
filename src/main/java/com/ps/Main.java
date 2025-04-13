588c6c713c1f8a4b6c73a26e29aa5cfa2e171cb3
Robyn <robyncab01@gmail.com>
2025-04-12 22:11:19 -0400

    Calculator 3 & formatting update

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,5 +1,6 @@
 package com.ps;
 import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
@@ -7,24 +8,23 @@ public class Main
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("FINANCIAL CALCULATOR");
-        System.out.println("1) Mortgage Calculator ");
+        System.out.println("1) Mortgage Calculator");
         System.out.println("2) CD Calculator");
+        System.out.println("3) Present Value of Annuity Calculator\n");
         System.out.print("Please Select a Calculator to use: ");
         int giveCommand = scanner.nextInt();
 
-        // The first two calculators need the principal/deposit, interest rate, and the duration of time
-        System.out.print("Enter the Principal Amount: ");
+        // All 3 calculators need the principal amount/deposit/monthly payout, interest rate, and the duration of years
+        System.out.print("\nEnter the Amount of Money: ");
         float principal = scanner.nextFloat();
 
         System.out.print("Enter the Interest Rate: ");
         float interestRate = (scanner.nextFloat() / 100);
 
-//        System.out.println("value of interest rate is: " + interestRate);
-
         System.out.print("Enter the Duration of Years: ");
         int duration = scanner.nextInt();
 
-        // Results are not put here because each case has 2 unique results
+        // Results are not put here because each case has 1 or 2 unique results
 
         switch(giveCommand){
             case 1:
@@ -33,18 +33,28 @@ public class Main
                 float moPayments = (float) (principal * ( moInterestRate * Math.pow((1+moInterestRate),numMoPayments)) / (Math.pow((1 + moInterestRate), numMoPayments ) - 1));
                 float totalInterest = (moPayments * numMoPayments) - principal;
 
-                System.out.println("The Total Monthly Payment is: $" + moPayments);
-                System.out.println("The Total Interest Rate is: $" + totalInterest);
+
+                System.out.printf("%nThe Total Monthly Payment is: $%.2f%n", moPayments);
+                System.out.printf("The Total Interest Rate is: $%.2f%n", totalInterest);
                 break;
-            case 2:
+            case 2: // WIP adding leap year calculations
                 float futureValue = (float) ( principal * Math.pow((1+(interestRate / 365)),(365 * duration)));
                 float interestEarned = (futureValue - principal);
 
-                System.out.println("The Future Value is: $" + futureValue);
-                System.out.println("The Interest Earned is: $" + interestEarned );
+
+                System.out.printf("%nThe Future Value is: $%.2f%n", futureValue);
+                System.out.printf("The Interest Earned is: $%.2f%n", interestEarned);
+                break;
+            case 3:
+                int totalNumPeriod = 12 * duration;
+                float periodInterestRate = interestRate / 12;
+                float presentValue = (float) (principal * ((1 - Math.pow((1 + periodInterestRate), -totalNumPeriod)) / periodInterestRate));
+
+
+                System.out.printf("%nThe Present Value of the Annuity is: $%.2f%n", presentValue);
                 break;
             default:
-                System.out.println("Invalid option");
+                System.out.println("\nInvalid option, Try Again");
         }
 
 
